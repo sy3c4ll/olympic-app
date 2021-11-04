a214cdf7227930841e50116d8d091e104c185e8c
sy3c4ll <sy3c4ll@gmail.com>
2021-11-04 22:35:21 +0900

    complete web app with jlooper's tutorial

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 import './style.css';
 // 🥇 import your json data
+import data from './olympic-sports.json';
 
 // 🥈 add a div reference here
+let div = document.getElementById('grid');
 
 for (var i = 0; i < data.length; i++) {
-	div.insertAdjacentHTML(
-		'beforeend',
-		'<div class="flip-card"><div class="flip-card-inner"><div class="flip-card-front"><img src="/' +
-			data[i].image +
-			'" alt=' +
-			JSON.stringify(data[i].sport) +
-			'><h2>' +
-			data[i].sport +
-			'</h2><h3>' +
-			data[i].details +
-			'</h3><h4>Favored for gold: ' +
-			data[i].gold +
-			'</h4></div><div class="flip-card-back"><h3 class="description">' +
-			data[i].description +
-			'</h3></div></div></div>'
-	);
+  div.insertAdjacentHTML(
+    'beforeend',
+    '<div class="flip-card"><div class="flip-card-inner"><div class="flip-card-front"><img src="/' +
+    data[i].image +
+    '" alt=' +
+    JSON.stringify(data[i].sport) +
+    '><h2>' +
+    data[i].sport +
+    '</h2><h3>' +
+    data[i].details +
+    '</h3><h4>Favored for gold: ' +
+    data[i].gold +
+    '</h4></div><div class="flip-card-back"><h3 class="description">' +
+    data[i].description +
+    '</h3></div></div></div>'
+  );
 }
